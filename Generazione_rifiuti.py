6b09a795df19ec1cb445e852e6bf2c835c33b61a
Beatrice Scomazzon <beatrice.scomazzon@studenti.unipd.it>
2025-01-09 15:40:09 +0100

    first commit

---
+++ Generazione_rifiuti.py
@@ -0,0 +1,288 @@
+import polars as pl
+import streamlit as st
+import altair as alt
+import geopandas as gpd
+
+
+st.write(" # Produzione di rifiuti in Europa")
+url=("https://ec.europa.eu/eurostat/databrowser/view/env_wasgen__custom_14750179/default/table?lang=en")
+
+st.write(f''' 
+### Introduzione e descrizione dei dati oggetto di studio
+La seguente analisi verterà sulla generazione di rifiuti da parte dei Paesi Europei, con particolare attenzione alle attività economiche coinvolte, alla pericolosità dei rifiuti e all' evoluzione nel tempo della quantità prodotta.
+I dati in seguito analizzati sono stati ottenuti dal [sito Eurostat]({url}).
+\nIl periodo di riferimento è strutturato ad intervalli biennali e va dal 2004 al 2022. Per il primo anno di riferimento, il 2004, gli Stati membri hanno potuto chiedere l'autorizzazione a non fornire parte delle informazioni riguardante i rifiuti 
+dei settori "Agricolture and fishing" e "Services", perciò per alcuni paesi questi valori sono mancanti.         
+
+
+Le informazioni sono disaggregate per:
+-  **fonti**: 19 attività commerciali secondo la classificazione NACE rev.2 e attività delle famiglie in ambito domestico.
+             La generazione di rifiuti è, quindi, attribuita ad attività di produzione o di consumo.
+-  **categorie** di rifiuti (secondo la Classificazione Europea dei Rifiuti a fini statistici). 
+
+Tutti i valori sono misurati sia in tonnellate di rifiuti che in kg pro-capite, sulla base della media annua della popolazione.
+\nGli Stati membri sono liberi di decidere sulle modalità di raccolta dei dati. Le opzioni generali sono: indagini, fonti amministrative, stime statistiche o una combinazione di metodi.
+
+Le informazioni sul trattamento dei rifiuti sono suddivise in 5 tipologie di trattamento (recupero, incenerimento con recupero energetico, 
+altri incenerimenti, smaltimento a terra e trattamento a terra) e in categorie di rifiuti.
+
+
+### Presentazione dei dati
+La tabella riporta i dati sulla produzione di rifiuti (in tonnellate) nei diversi settori economici per ogni paese europeo.
+
+Per comodità di rappresentazione si sono qui considerati i rifiuti pericolosi e non pericolosi nel loro totale.
+
+L'esclusione dell'Albania è dovuta alla quasi totale assenza di dati in ogni anno/attività, per cui sarebbe risultato difficile fare un'analisi utile. 
+
+
+         ''')
+
+
+start_data = (pl
+            .read_csv("Gen_of_waste_all.csv", separator=",", null_values=[":", ""])
+            .drop(pl.col("DATAFLOW","LAST UPDATE", "OBS_FLAG", "freq"))
+            .select(pl.col("geo", "unit", "hazard", "nace_r2","TIME_PERIOD", "OBS_VALUE", "waste")
+            .filter(pl.col("geo") != "Albania", 
+                    pl.col("waste") == "Total waste")))
+
+@st.cache_data    
+def table():
+    return (      
+    start_data
+    .filter(pl.col("hazard") == "Hazardous and non-hazardous - total",
+            pl.col("unit") == "Tonne")
+    .select(pl.col("*").exclude("hazard", "unit", "waste"))
+    .pivot(on="TIME_PERIOD", values="OBS_VALUE")
+    .sort(by="geo")
+    )
+st.write(table())
+
+data= (
+    table()
+    .unpivot(index=["geo", "nace_r2"], 
+                      value_name="values", 
+                      variable_name="year")
+    .filter(pl.col("geo")!= "European Union - 27 countries (from 2020)",
+            pl.col("geo") != "European Union - 28 countries (2013-2020)")
+    .sort(["nace_r2", "geo", "year"])  
+    )
+
+
+# non serve metterli ma è da scrivere sul read i tentativi fatti !!!!
+prev_data= (
+    data
+    .group_by(["geo","nace_r2"])
+    .agg(pl.col("values").fill_null(strategy="forward").alias("previsioni"))
+    .sort(["geo","nace_r2"])
+)
+#new_data= data.merge(prev_data, on="geo", how="left")
+
+
+
+
+st.write("""
+### Come si è evoluta la produzione di rifiuti nei vari stati nel corso degli anni?
+
+Il seguente grafico consente di selezionare uno o più stati, osservandone l'evoluzione individuale, ma anche permettendo di effettuare un confronto nel tempo.
+         
+I valori per ogni anno corrispondo alla somma di rifiuti prodotti per ogni settore.
+
+Posizionandosi con il cursore sopra alle linee è possibile visualizzare "un'etichetta" che riporta l'anno considerato, il nome del paese e il corrispettivo valore di rifiuti totali. 
+""")
+
+
+def time_evolution():
+    countries = data.select("geo").unique().sort("geo")
+    selected_country = st.multiselect(
+        "Seleziona uno o più stati",
+        countries,
+        default="Italy"
+    )
+    st.line_chart(
+        data 
+        .filter(pl.col("nace_r2") == "All NACE activities plus households")
+        .with_columns(pl.col("year").cast(pl.Int64))
+        .filter(pl.col("geo").is_in(selected_country)),
+        x="year",
+        y="values",
+        color="geo"
+    )
+
+st.write(time_evolution())
+
+
+st.write('''
+### Quali sono gli stati che producono la maggiore quantità di rifiuti in un dato anno (in tonnellate)?
+#### Confronto tra stati in base alla *pericolosità* dei rifiuti generati complessivamente da tutte le attività economiche 
+Il seguente grafico evidenzia quali sono gli stati che producono la maggiore quantità di scarti considerando la somma delle 
+tonnellate prodotte da ogni settore economico in riferimento all'anno selezionato .
+
+         ''' )
+
+def select_year(key):
+    years = [2004, 2006, 2008, 2010, 2012, 2014, 2016, 2018, 2020, 2022]
+    year = st.pills("Seleziona l'anno", years, default=2022, key=key)
+    return year
+
+def hazardness():
+    year= select_year("key0")
+    datayear= (
+        start_data
+            .filter(pl.col("hazard") != "Hazardous and non-hazardous - total",
+                    pl.col("unit") == "Tonne",
+                    pl.col("nace_r2")!=  "All NACE activities plus households",
+                    pl.col("geo")!= "European Union - 27 countries (from 2020)",
+                    pl.col("geo") != "European Union - 28 countries (2013-2020)")
+            .select(pl.col("*").exclude("unit", "waste", "nace_r2"))
+            .with_columns(pl.col("TIME_PERIOD").cast(pl.Int64))
+            .filter(pl.col("TIME_PERIOD")== year)
+    )
+    st.altair_chart(
+        alt.Chart(datayear)
+        .mark_bar()
+        .encode(
+            x=alt.X('sum(OBS_VALUE):Q', title='Total waste'),
+            y=alt.Y('geo:N', title='Country', sort="-x"),
+            color=alt.Color('hazard:N', legend=alt.Legend(title="Hazard"))
+        ),
+        use_container_width=True
+    )
+st.write(hazardness())
+
+
+st.write('''
+### Generazione di rifiuti pro-capite (in kg)
+Normalizzare i dati sulla produzione di rifiuti utilizzando la popolazione media di ciascun paese per ogni anno considerato permette di ottenere 
+valori che rendono possible il confronto tra paesi con dimensioni demografiche diverse.
+         ''')
+
+def select_activity(key):
+    activities= data.select("nace_r2").unique().sort("nace_r2")
+    activity= st.selectbox("Seleziona il settore", activities, key= key)
+    return activity
+
+
+year= select_year("key1")
+nace= select_activity("key2")
+
+data_kg= (start_data
+            .filter(pl.col("unit") == "Kilograms per capita",
+                pl.col("hazard") == "Hazardous and non-hazardous - total",
+                pl.col("geo")!= "European Union - 27 countries (from 2020)",
+                pl.col("geo") != "European Union - 28 countries (2013-2020)",
+                pl.col("nace_r2") == nace,
+                pl.col("TIME_PERIOD") == year)
+            .select(pl.col("*").exclude("hazard", "unit", "waste"))
+            .pivot(on="TIME_PERIOD", values= "OBS_VALUE")
+            .unpivot(index=["geo", "nace_r2"], 
+                    value_name="kg_pro_capite", 
+                    variable_name="year")
+            .with_columns(pl.col("kg_pro_capite").cast(pl.Float64))
+            .sort(["nace_r2", "geo", "year"])
+        )
+base = alt.Chart(data_kg).encode(
+    x='kg_pro_capite:Q',
+    y=alt.Y("geo:O", sort="-x"),
+    text='kg_pro_capite'
+)
+base.mark_bar() + base.mark_text(align='left', dx=2)
+
+st.write('''
+    ### Cartina dell'Europa: massima produzione di rifiuti totale pro-capite (in kg) per settore
+
+    ** commento ***
+         ''')
+
+@st.cache_data   
+def get_geography():
+    url = "https://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_0_countries.zip"
+    dati = gpd.read_file(url)
+    return dati
+
+world= get_geography()
+data = world.merge(data_kg.to_pandas(), left_on="ADMIN", right_on="geo")
+
+def add_map(chart):
+    chart.save("map.html")
+    with open("map.html") as fp:
+        st.components.v1.html(fp.read(), width=800, height=800)
+world = (
+    alt.Chart(world)
+    .properties(width=600, height=600)
+    .mark_geoshape(color="lightgray", stroke="white")
+)
+chart = (
+    alt.Chart(data)
+    .properties(width=600, height=600)
+    .mark_geoshape(stroke="white")
+    .encode(
+        alt.Color("kg_pro_capite").scale(scheme="yelloworangered", type="sqrt"), 
+        alt.Tooltip(["ADMIN", "kg_pro_capite"])
+    )
+)
+complete = (world + chart).project(  
+        type="azimuthalEqualArea",
+        scale=700,
+        center=(10, 48)
+)
+add_map(complete)
+
+
+
+st.write('''
+### Che tipo di correlazione c'è tra il valore del PIL (Prodotto Interno Lordo) e la quantità di rifiuti prodotti?
+Il seguente grafico di dispersione rappresenta la correlazione tra la crescita economica pro capite e la produzione di rifiuti pro capite (in kg). 
+Il legame è evidenziato da una retta di regressione.         
+         ''')
+
+@st.cache_data
+def pil_table():
+    return(
+        pl
+        .read_csv("Real_GDP_per_capita.csv", separator=",", null_values=[":", ""], schema_overrides= {"OBS_VALUE": pl.Float64})
+        .drop(pl.col("DATAFLOW", "LAST UPDATE","freq",  "OBS_FLAG"))
+        .select(pl.col("geo", "TIME_PERIOD", "unit", "na_item", "OBS_VALUE"))
+        .filter(pl.col("unit")== "Chain linked volumes (2010), euro per capita",
+                pl.col("TIME_PERIOD").is_in([2004, 2006, 2008, 2010, 2012, 2014, 2016, 2018, 2020, 2022]) )
+        .with_columns(pl.col("OBS_VALUE").cast(pl.Float64))
+        .select(pl.col("*").exclude("na_item", "unit"))
+        .pivot(on="TIME_PERIOD", values="OBS_VALUE")
+        .unpivot(index="geo", value_name= "GDP_per_capita", variable_name="year")
+    )
+
+year2=select_year("key3")
+data_kg2= (start_data
+            .filter(pl.col("unit") == "Kilograms per capita",
+                pl.col("hazard") == "Hazardous and non-hazardous - total",
+                pl.col("nace_r2") == "All NACE activities plus households",
+                pl.col("TIME_PERIOD")== year2)
+            .select(pl.col("*").exclude("hazard", "unit", "waste"))
+            .pivot(on="TIME_PERIOD", values= "OBS_VALUE")
+            .unpivot(index=["geo", "nace_r2"], 
+                    value_name="kg_pro_capite", 
+                    variable_name="year")
+            .with_columns(pl.col("kg_pro_capite").cast(pl.Float64))
+        )
+
+GDP_waste=(
+    data_kg2.join(pil_table(), on=["geo", "year"], how="inner")
+)
+
+line= alt.Chart(GDP_waste).mark_point().encode(
+        x='GDP_per_capita:Q',
+        y='kg_pro_capite').transform_regression('GDP_per_capita', 'kg_pro_capite').mark_line(color="red")
+
+points= alt.Chart(GDP_waste).mark_point().encode(
+        x='GDP_per_capita:Q',
+        y='kg_pro_capite:Q',
+        tooltip=["geo:N", "kg_pro_capite", "GDP_per_capita"]
+        ).properties(
+            width=600,
+            height=400 
+        )
+
+points+line
+# scrivere mettendo in evidenza gli outliers e l'effetto che hanno sulla correlzaione
+
+
+# da provare eventualmente grafico di distribuzione, anche se i dati non si prestano particolarmente 
